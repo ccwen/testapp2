4844a097ba060d054b72a97407aefb94e86a8184
yapcheahshen <yapcheahshen@gmail.com>
2015-01-15 22:35:32 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,42 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "testapp2",
+ "minruntime": 4,
+ "baseurl": "http://rawgit.com/ksanaforge/testapp2/master/",
+ "description": "",
+ "files": [
+  "index.html",
+  "index.css",
+  "bundle.js",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  552,
+  21,
+  142395,
+  277980,
+  625422,
+  321,
+  35601,
+  113498,
+  97570
+ ],
+ "filedates": [
+  "2015-01-15T14:35:05.000Z",
+  "2015-01-15T14:35:05.000Z",
+  "2015-01-15T14:35:09.000Z",
+  "2015-01-15T14:35:05.000Z",
+  "2015-01-15T14:35:05.000Z",
+  "2015-01-15T14:35:05.000Z",
+  "2015-01-15T14:35:05.000Z",
+  "2015-01-15T14:35:05.000Z",
+  "2015-01-15T14:35:07.000Z"
+ ],
+ "date": "2015-01-15T14:35:10.585Z"
+})
